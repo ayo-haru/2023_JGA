e64849376f458d7f6c4e9366e58c8709b85b57a9
itida <maimai130806@gmail.com>
2023-03-21 17:33:35 +0900

    飼育員に出てたエラーもうでないはず

--- JGA/Assets/Project/Scripts/Manager/StageSceneManager.cs
+++ JGA/Assets/Project/Scripts/Manager/StageSceneManager.cs
@@ -8,6 +8,8 @@
 // [Date]
 // 2023/02/27	スクリプト作成
 // 2023/03/16	スポーン地点をPlayerRespwanに変更(吉原)
+// 2023/03/20	飼育員自動生成(伊地田)
+// 2023/03/21	飼育員自動生成バグとり(伊地田)
 //=============================================================================
 using System;
 using System.Collections;
@@ -56,24 +58,29 @@ public class StageSceneManager : BaseSceneManager {
         playerInstance = Instantiate(playerObj,playerRespawn.transform.position,Quaternion.Euler(0.0f,5.0f,0.0f));
 
         //----- 飼育員の生成 -----
-        ZooKeeperData.Data[] _list = MySceneManager.GameData.zooKeeperData.list;
-        GameObject zooKeeperObj = PrefabContainerFinder.Find(MySceneManager.GameData.characterDatas, "ZooKeeper.prefab");
-        GameObject parent = GameObject.Find("ZooKeepers");
+        ZooKeeperData.Data[] _list = MySceneManager.GameData.zooKeeperData.list;    // 設定された人数分を生成する
+        GameObject zooKeeperObj = PrefabContainerFinder.Find(MySceneManager.GameData.characterDatas, "ZooKeeper.prefab");   // 生成するオブジェクト
+        GameObject parent = GameObject.Find("ZooKeepers");  //  生成するときの親にするオブジェクト
         for (int i = 0; i < _list.Length; i++) {
-            GameObject spawnPos = GameObject.Find(_list[i].name + "Spawn");
-            if (spawnPos == null) {
-                Debug.LogError(_list[i].name + "のスポーン位置が見つかりませんでした。(StageSceneManager.cs)");
+            GameObject spawnPos = GameObject.Find(_list[i].name + "Spawn"); // 生成位置を名前で取得する
+            if (spawnPos == null) { // 存在するか
+                Debug.LogError(_list[i].name + "のスポーン位置が見つかりませんでした。(StageSceneManager.cs)");    // 存在しないのでメッセージ出す
             } else {
-                _list[i].respawnTF = spawnPos.GetComponent<Transform>();
+                _list[i].respawnTF = spawnPos.GetComponent<Transform>();    // 存在してたので位置を取得
             }
 
-            GameObject zooKeeperInstace = Instantiate(zooKeeperObj, spawnPos.transform.position, Quaternion.identity);
+            // 設定された定数から実際のpositionを入れる
             for (int j = 0; j < _list[i].roots.Length; j++) {
-                _list[i].rootTransforms.Add(zooKeeperRootPos[(int)_list[i].roots[j]]);
+                _list[i].rootTransforms[i] = zooKeeperRootPos[(int)_list[i].roots[j]];
             }
 
+            // 生成
+            GameObject zooKeeperInstace = Instantiate(zooKeeperObj, spawnPos.transform.position, Quaternion.identity);
+            zooKeeperInstace.transform.parent = parent.transform;   // 親を設定
+
+            // データをZooKeeperAI.csに流し込む
             zooKeeperInstace.GetComponent<ZooKeeperAI>().SetData(_list[i]);
-            zooKeeperInstace.transform.parent = parent.transform;
+                
         }
 
 
