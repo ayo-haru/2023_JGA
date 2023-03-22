388079ddc1791281a09359f604e45b9418f6a04f
itida <maimai130806@gmail.com>
2023-03-22 19:34:13 +0900

    プロトタイプのシーンでポーズできますは嘘でした。ほんとにできます。

--- JGA/Assets/Project/Scripts/Manager/StageSceneManager.cs
+++ JGA/Assets/Project/Scripts/Manager/StageSceneManager.cs
@@ -110,14 +110,14 @@ public class StageSceneManager : BaseSceneManager {
         }
 
 
-        // プロトタイプ用
-        if (Input.GetKeyDown(KeyCode.Escape)){
-#if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
-#else
-    Application.Quit();//ゲームプレイ終了
-#endif
-        }
+//        // プロトタイプ用
+//        if (Input.GetKeyDown(KeyCode.Escape)){
+//#if UNITY_EDITOR
+//            UnityEditor.EditorApplication.isPlaying = false;//ゲームプレイ終了
+//#else
+//    Application.Quit();//ゲームプレイ終了
+//#endif
+//        }
     }
 
     private void LateUpdate() {
