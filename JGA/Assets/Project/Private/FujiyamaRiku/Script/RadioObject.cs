6c1ff69ba3c441e347df72fb1ca70e213df27ffd
itida <maimai130806@gmail.com>
2023-04-24 17:58:57 +0900

    敗北者じゃけぇ

--- JGA/Assets/Project/Private/FujiyamaRiku/Script/RadioObject.cs
+++ JGA/Assets/Project/Private/FujiyamaRiku/Script/RadioObject.cs
@@ -18,27 +18,22 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UniRx;
 
-public class RadioObject : BaseObj, IObjectSound
-{
-	//ペンギンが持った後に落とすときまでのフラグ
-	private bool fallFlg;
-	//ラジオをオンオフするフラグ
-	private bool onOffFlg = false;
-	//ラジオが鳴ってる時用のAudioSource
-	private AudioSource[] playAudio;
-	//ラジオのオーディオを指定
-	const int RadioAudio = 1;
-	//ラジオ特有のポーズ処理
-	private bool pauseFlg = false;
+public class RadioObject : BaseObj, IObjectSound {
+    //ペンギンが持った後に落とすときまでのフラグ
+    private bool fallFlg;
+    //ラジオをオンオフするフラグ
+    private bool onOffFlg;
+    //ラジオが鳴ってる時用のAudioSource
+    private AudioSource[] playAudio;
+    //ラジオのオーディオを指定
+    const int RadioAudio = 1;
+    //ラジオ特有のポーズ処理
+    private bool pauseFlg;
 
-	// プレイヤーと当たっているかどうか
-	private bool isTouchPlayer = false;
-
-	/// <summary>
-	/// Prefabのインスタンス化直後に呼び出される：ゲームオブジェクトの参照を取得など
-	/// </summary>
-	protected override void Awake()
-	{
+    /// <summary>
+    /// Prefabのインスタンス化直後に呼び出される：ゲームオブジェクトの参照を取得など
+    /// </summary>
+    protected override void Awake() {
         Init();
         objType = ObjType.HIT_HOLD;
         //生成＆初期化
@@ -46,100 +41,66 @@ public class RadioObject : BaseObj, IObjectSound
     }
 
 
-	/// <summary>
-	/// 一定時間ごとに呼び出されるメソッド（端末に依存せずに再現性がある）：rigidbodyなどの物理演算
-	/// </summary>
-	void FixedUpdate()
-	{
-	}
-
-	/// <summary>
-	/// 1フレームごとに呼び出される（端末の性能によって呼び出し回数が異なる）：inputなどの入力処理
-	/// </summary>
-	void Update()
-	{
-		if(isTouchPlayer && player.IsHit) {	// プレイヤーと当たっていてはたいたフラグが立ったら
-			onOffFlg = !onOffFlg;
-
-			if (onOffFlg) {
-                playAudio[RadioAudio].Stop();
-
-                SoundManager.Play(audioSource, SoundManager.ESE.RADIO_RELEASE);
-			} else {
-                SoundManager.Play(audioSource, SoundManager.ESE.RADIO_CATCH);
-            }
-		}
-		Debug.Log("onoff"+onOffFlg);
-		Debug.Log("istouch"+isTouchPlayer);
-		isTouchPlayer = false;
-		
-
-		//SEが鳴ってるときになっているフラグを返す
-		PlaySoundChecker();
-        //ラジオが鳴っているときの処理
-        if (playAudio[RadioAudio].isPlaying && !pauseFlg)
-		{
-			isPlaySound = true;
-		}
-		if (!playAudio[RadioAudio].isPlaying && !pauseFlg)
-		{
-			//ラジオがオンになったときにSEが終わったらラジオを流す
-			if (onOffFlg && !audioSource.isPlaying)
-			{
-				SoundManager.Play(playAudio[RadioAudio], SoundManager.ESE.RADIO_PLAY);
-			}
-			isPlaySound = false;
-		}
+    /// <summary>
+    /// 一定時間ごとに呼び出されるメソッド（端末に依存せずに再現性がある）：rigidbodyなどの物理演算
+    /// </summary>
+    void FixedUpdate() {
     }
 
-    protected override void OnCollisionEnter(Collision collison)
-	{
-		//プレイヤーと当たっていてプレイヤーが持っていなかったら
-		if (collison.gameObject.tag == "Player" && !fallFlg)
-		{
-			SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_HIT);
-		}
-		//プレイヤーがはたいたときにOnOffする
-		if (collison.gameObject.tag == "Player") {
+    /// <summary>
+    /// 1フレームごとに呼び出される（端末の性能によって呼び出し回数が異なる）：inputなどの入力処理
+    /// </summary>
+    void Update() {
+        //SEが鳴ってるときになっているフラグを返す
+        PlaySoundChecker();
+        //ラジオが鳴っているときの処理
+        if (playAudio[RadioAudio].isPlaying && !pauseFlg) {
+            isPlaySound = true;
+        }
+        if (!playAudio[RadioAudio].isPlaying && !pauseFlg) {
+            //ラジオがオンになったときにSEが終わったらラジオを流す
+            if (onOffFlg && !audioSource.isPlaying) {
+                SoundManager.Play(playAudio[RadioAudio], SoundManager.ESE.RADIO_PLAY);
+            }
+            isPlaySound = false;
+        }
+    }
 
-			//if (onOffFlg) {
-			//    onOffFlg = false;
-
-			//    playAudio[RadioAudio].Stop();
-
-			//    SoundManager.Play(audioSource, SoundManager.ESE.RADIO_RELEASE);
-
-			//} else if (!onOffFlg) {
-			//    onOffFlg = true;
-
-			//    SoundManager.Play(audioSource, SoundManager.ESE.RADIO_CATCH);
-
-			//}
-			isTouchPlayer = true;
+    protected override void OnCollisionEnter(Collision collison) {
+        //プレイヤーと当たっていてプレイヤーが持っていなかったら
+        if (collison.gameObject.tag == "Player" && !fallFlg) {
+            SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_HIT);
         }
 
         //地面に当たったときにプレイヤーが持っている状態から落としたら
-        if (collison.gameObject.tag == "Ground" && fallFlg)
-		{
+        if (collison.gameObject.tag == "Ground" && fallFlg) {
             PlayRelease();
-			fallFlg = false;
-		}
-	}
-
-    new private void OnCollisionExit(Collision collision) {
-		if (collision.gameObject.tag == "Player") {
-			//isTouchPlayer = false;
-		}
+            fallFlg = false;
+        }
     }
 
-    new private void OnCollisionStay(Collision other)
-	{
+    protected override void OnTriggerStay(Collider other) {
+        //プレイヤーがはたいたときにOnOffする
+        if (player.IsHit && other.tag == "Player") {
+            if (onOffFlg) {
+                onOffFlg = false;
+
+                playAudio[RadioAudio].Stop();
+
+                SoundManager.Play(audioSource, SoundManager.ESE.RADIO_RELEASE);
+
+            } else if (!onOffFlg) {
+                onOffFlg = true;
+
+                SoundManager.Play(audioSource, SoundManager.ESE.RADIO_CATCH);
+
+            }
+        }
+
         //プレイヤーの判定に触れているときに
-        if (other.gameObject.tag == "Player")
-        {
+        if (other.tag == "Player") {
             //持っている判定だったら
-            if (player.IsHold && !fallFlg)
-            {
+            if (player.IsHold && !fallFlg) {
                 PlayPickUp();
                 fallFlg = true;
             }
@@ -147,42 +108,37 @@ public class RadioObject : BaseObj, IObjectSound
         }
     }
 
-	public void PlayPickUp()
-	{
-		SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_HIT);
-	}
+    public void PlayPickUp() {
+        SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_HIT);
+    }
 
-	public void PlayHold()
-	{
+    public void PlayHold() {
 
-	}
+    }
 
-	public void PlayRelease()
-	{
-		SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_DROP);
-	}
+    public void PlayRelease() {
+        SoundManager.Play(audioSource, SoundManager.ESE.OBJECT_DROP);
+    }
 
-	/// <summary>
+    /// <summary>
 	/// ラジオの音を止める
 	/// </summary>
 	public void StopRadio() {
-		playAudio[RadioAudio].Stop();
-		onOffFlg = false;
+        playAudio[RadioAudio].Stop();
+        onOffFlg = false;
     }
 
-    protected override void Pause()
-    {
+    protected override void Pause() {
         // 物理挙動停止
         audioSource.Pause();
         playAudio[RadioAudio].Pause();
         rb.velocity = pauseVelocity;
         rb.angularVelocity = pauseAngleVelocity;
         rb.isKinematic = false;
-		pauseFlg = true;
+        pauseFlg = true;
     }
 
-    protected override void Resumed()
-    {
+    protected override void Resumed() {
         audioSource.Play();
         playAudio[RadioAudio].Play();
         // 物理挙動開始
