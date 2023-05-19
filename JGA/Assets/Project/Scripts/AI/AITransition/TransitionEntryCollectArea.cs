21654c9cacfd702f3990522c8f0d0003b0ed77a2
OgusuYuuko <yuuko_ogusu@outlook.jp>
2023-05-19 13:35:33 +0900

    ペンギンエリア到着を当たり判定を使った処理に変更。座る客増やした

---
+++ JGA/Assets/Project/Scripts/AI/AITransition/TransitionEntryCollectArea.cs
@@ -0,0 +1,85 @@
+//=============================================================================
+// @File	: [TransitionEntryCollectArea.cs]
+// @Brief	: 遷移条件　CollectAreaに到着したか？
+// @Author	: Ogusu Yuuko
+// @Editer	: 
+// @Detail	: 
+// 
+// [Date]
+// 2023/05/19	スクリプト作成
+//=============================================================================
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionEntryCollectArea : AITransition
+{
+    //CollectAreaと接触したか？
+    private bool bEntry;
+#if false
+    /// <summary>
+    /// Prefabのインスタンス化直後に呼び出される：ゲームオブジェクトの参照を取得など
+    /// </summary>
+    void Awake()
+	{
+		
+	}
+
+	/// <summary>
+	/// 最初のフレーム更新の前に呼び出される
+	/// </summary>
+	void Start()
+	{
+		
+	}
+
+	/// <summary>
+	/// 一定時間ごとに呼び出されるメソッド（端末に依存せずに再現性がある）：rigidbodyなどの物理演算
+	/// </summary>
+	void FixedUpdate()
+	{
+		
+	}
+
+	/// <summary>
+	/// 1フレームごとに呼び出される（端末の性能によって呼び出し回数が異なる）：inputなどの入力処理
+	/// </summary>
+	void Update()
+	{
+		
+	}
+#endif
+
+    public override void InitTransition()
+    {
+        bEntry = false;
+    }
+
+    public override bool IsTransition()
+    {
+        return bEntry;
+    }
+
+    public override bool ErrorCheck()
+    {
+        return true;
+    }
+#if true
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag != "CollectPoint") return;
+        bEntry = true;
+    }
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag != "CollectPoint") return;
+        bEntry = true;
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "CollectPoint") return;
+        bEntry = false;
+    }
+#endif
+}
