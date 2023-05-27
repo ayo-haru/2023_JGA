338a02f0db04d922b647db25f1dc3165bc9a2a8f
YANIMAX <95224802+hal-sakai@users.noreply.github.com>
2023-05-27 14:48:51 +0900

    勝手にUnityEditor追加しやがっていた問題を修正。

--- JGA/Assets/Project/Scripts/Player/Player.cs
+++ JGA/Assets/Project/Scripts/Player/Player.cs
@@ -17,7 +17,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UniRx;
 using Unity.VisualScripting;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
